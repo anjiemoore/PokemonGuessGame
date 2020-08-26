1de0b14ab0cf67620c0ae75b88883a14743ef411
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-08-26 15:30:50 -0600

    changed functions and made components, able to dynamically render image

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
 import './App.css';
+import loading from './loading.gif';
 
 
 const TOTAL_NUM_POKEMON = 893;
-const newPokemon = async () => {
-  let imageURL = null;
-
-  while (!imageURL) {
-    const randomNumber = Math.floor(Math.random() * TOTAL_NUM_POKEMON);
-    const endpoint = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
-    console.log(endpoint);
-    const data = await (await fetch(endpoint)).json();
-    imageURL = data.sprites.other["official-artwork"].front_default;
-  }
-  console.log(imageURL);
-};
 
 class Game extends React.Component {
   constructor(props: any) {
     super(props);
     this.state = {imageURL: null};
   }
+
+  async newPokemon() {
+    let imageURL = null;
+    
+    this.setState({imageURL: loading});
+    while (!imageURL) {
+      const randomNumber = Math.floor(Math.random() * TOTAL_NUM_POKEMON);
+      const endpoint = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
+      console.log(endpoint);
+      const data = await (await fetch(endpoint)).json();
+      imageURL = data.sprites.other["official-artwork"].front_default;
+    }
+    console.log(imageURL);
+    this.setState({imageURL});
+  }
+
   render() {
     return (
       <div className="App">
-        <img src={this.state.imageURL}></img>
-        <button className="start" onClick={newPokemon}>Start Game</button>
+        <img alt="" src={this.state.imageURL}></img>
+        <button className="start" onClick={() => this.newPokemon()}>Start Game</button>
       </div>
     );  
   }
