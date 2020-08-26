2b2df2c827059b4643d4986bcccf1a791f706b31
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-08-26 17:05:13 -0600

    added name to be displayed on show click

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,30 @@ import loading from './loading.gif';
 
 const TOTAL_NUM_POKEMON = 893;
 
-class Game extends React.Component<{}, {imageURL: string, silhouette: boolean}> {
+class Game extends React.Component<{}, {imageURL: string, silhouette: boolean, display: boolean, name: string}> {
   constructor(props: any) {
     super(props);
-    this.state = {imageURL: "", silhouette: true};
+    this.state = {imageURL: "", silhouette: true, display: true, name: ""};
   }
 
   async newPokemon() {
     let imageURL = null;
-    
-    this.setState({imageURL: loading, silhouette: false});
+    let name = null;
+    this.setState({imageURL: loading, silhouette: false, name: ""});
     while (!imageURL) {
       const randomNumber = Math.floor(Math.random() * TOTAL_NUM_POKEMON);
       const endpoint = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
       console.log(endpoint);
-      const data = await (await fetch(endpoint)).json();
-      imageURL = data.sprites.other["official-artwork"].front_default;
+      try {
+        const data = await (await fetch(endpoint)).json();
+        imageURL = data.sprites.other["official-artwork"].front_default;
+        name = data.name.split("-")[0].toUpperCase();
+      } catch {
+        continue;
+      }
     }
     console.log(imageURL);
-    this.setState({imageURL, silhouette: true});
+    this.setState({imageURL, silhouette: true, display: false, name});
   }
 
   render() {
@@ -32,9 +37,17 @@ class Game extends React.Component<{}, {imageURL: string, silhouette: boolean}> 
     }
     return (
       <div className="App">
-        <img className="image" style={this.state.silhouette ? style : undefined} alt="" src={this.state.imageURL}></img>
+        <img 
+        className="image" 
+        onLoad={() => this.setState({display: true})} 
+        style={{filter: `${this.state.silhouette ? "contrast(0) brightness(0)" : ""}`, display: `${this.state.display ? "inline-block" : "none"}`}} 
+        alt="" src={this.state.imageURL}></img>
+        <br></br>
+        <h1 style={{display: `${!this.state.silhouette ? "inline-block" : "none"}`}}>{this.state.name}</h1>
+        <br></br>
+        <br></br>
         <button className="start" onClick={() => this.newPokemon()}>Start Game</button>
-        <button className="start" onClick={() => this.setState({silhouette: false})}>Reveal</button>
+        <button className="start" onClick={() => this.setState({silhouette: !this.state.silhouette})}>{this.state.silhouette ? "Reveal" : "Hide"}</button>
 
       </div>
     );  
