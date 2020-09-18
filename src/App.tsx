a6c1ed8aef1ef1ca76d9ab237a31ef80af871288
anjiemoore <34924909+anjiemoore@users.noreply.github.com>
2020-09-18 16:25:03 -0600

    eventhandler on textbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import loading from './loading.gif';
 
-
 const TOTAL_NUM_POKEMON = 893;
 
-class Game extends React.Component<{}, { imageURL: string, silhouette: boolean, display: boolean, name: string, userGuess: string }> {
+class Game extends Component<{}, { imageURL: string, silhouette: boolean, display: boolean, pokeName: string, userGuess: string }> {
   constructor(props: any) {
     super(props);
-    this.state = { imageURL: "", silhouette: true, display: true, name: "", userGuess: "" };
+    this.state = { imageURL: "", silhouette: true, display: true, pokeName: "", userGuess: "" };
   }
 
   handleGuessChange(e: any) {
+    const guess = e.currentTarget.value.toUpperCase();
     this.setState({
-      userGuess: e.currentTarget.value
+      userGuess: guess
     });
+    if (guess === this.state.pokeName) {
+      this.setState({
+        silhouette: false
+      });
+    }
   };
 
   async newPokemon() {
     let imageURL = null;
-    let name = null;
-    this.setState({ imageURL: loading, silhouette: false, name: "" });
+    let pokeName = null;
+    this.setState({ imageURL: loading, silhouette: false, pokeName: "", userGuess: "" });
     while (!imageURL) {
       const randomNumber = Math.floor(Math.random() * TOTAL_NUM_POKEMON);
       const endpoint = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
@@ -28,15 +33,14 @@ class Game extends React.Component<{}, { imageURL: string, silhouette: boolean, 
       try {
         const data = await (await fetch(endpoint)).json();
         imageURL = data.sprites.other["official-artwork"].front_default;
-        name = data.name.split("-")[0].toUpperCase();
+        pokeName = data.name.split("-")[0].toUpperCase();
       } catch {
         continue;
       }
     }
     console.log(imageURL);
-    this.setState({ imageURL, silhouette: true, display: false, name });
+    this.setState({ imageURL, silhouette: true, display: false, pokeName });
   }
-
   render() {
     return (
       <div className='App'>
@@ -44,24 +48,25 @@ class Game extends React.Component<{}, { imageURL: string, silhouette: boolean, 
           className="image"
           onLoad={() => this.setState({ display: true })}
           style={{ filter: `${this.state.silhouette ? "contrast(0) brightness(0)" : ""}`, display: `${this.state.display ? "inline-block" : "none"}` }}
-          alt="" src={this.state.imageURL}></img>
+          alt="" src={this.state.imageURL}>
+        </img>
         <br></br>
-        <h1 style={{ display: `${!this.state.silhouette ? "inline-block" : "none"}` }}>{this.state.name}</h1>
+        <h1 style={{ display: `${!this.state.silhouette ? "inline-block" : "none"}` }}>{this.state.pokeName}</h1>
         <br></br>
         <br></br>
         <input type="text" name="userGuess" value={this.state.userGuess} onChange={(e) => this.handleGuessChange(e)} />
-        <button className="submitbtn">Guess!</button>
-        <h1>{this.state.userGuess}</h1>
+        <br></br>
+        <br></br>
         <button className="start" onClick={() => this.newPokemon()}>Start Game</button>
         <button className="start" onClick={() => this.setState({ silhouette: !this.state.silhouette })}>{this.state.silhouette ? "Reveal" : "Hide"}</button>
-
       </div>
     );
   }
 }
 
 function App() {
-  return (<Game />);
+  return (
+  <div><Game /><Game /></div>);
 }
 
 export default App;
